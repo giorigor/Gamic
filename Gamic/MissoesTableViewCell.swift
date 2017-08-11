a896372887e89263e211c34753b7d37475e1cf26
Eric Ribeiro <ericribeiro@outlook.com.br>
2017-08-11 19:38:52 -0300

    O view da tela "Missões" foi finalizado

---
+++ Gamic/MissoesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MissoesTableViewCell.swift
+//  Gamic
+//
+//  Created by Eric Ribeiro on 11/08/17.
+//  Copyright © 2017 Eric Ribeiro. All rights reserved.
+//
+
+import UIKit
+
+class MissoesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
